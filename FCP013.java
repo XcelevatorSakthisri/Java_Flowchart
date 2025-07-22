f8676b798097ea7efdf03fea3910ffdc46b65fb6
Sri here <sakthisri.2534@xcelevate.org>
2025-07-22 22:31:22 +0530

    Add files via upload

---
+++ FCP013.java
@@ -0,0 +1,18 @@
+public class FCP013{
+public static void main(String[] args){
+int N=5;
+int R=0;
+int A=0;
+R=N%2;
+if (R==0)
+{
+A=R*2;
+System.out.println(A);
+}
+else
+{
+A=N*2;
+System.out.println(A);
+}
+}
+}
