20b4c2898f5753722cd54d6b91fff27d50f56725
Sri here <sakthisri.2534@xcelevate.org>
2025-07-22 22:31:39 +0530

    Add files via upload

---
+++ FCP014.java
@@ -0,0 +1,25 @@
+public class FCP014{
+public static void main(String[] args){
+int N1=90;
+int N2=40;
+int N3=30;
+int MX=0;
+if (N1>N2)
+{
+MX=N1;
+}
+else
+{
+MX=N2;
+}
+if (MX>N3)
+{
+MX=MX;
+}
+else
+{
+MX=N3;
+}
+System.out.println(MX);
+}
+}
