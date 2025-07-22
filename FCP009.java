9802032f1243ca5492ade74740b0863a519c957c
Sri here <sakthisri.2534@xcelevate.org>
2025-07-22 22:28:54 +0530

    Add files via upload

---
+++ FCP009.java
@@ -0,0 +1,20 @@
+public class FCP009{
+public static void main(String[] args){
+int A=15;
+int B=4;
+int C=0;
+int R=0;
+if (A>B)
+{
+R=A/B;
+C=A-R;
+System.out.println(C);
+}
+else
+{
+R=B/A;
+C=B-R;
+System.out.println(C);
+}
+}
+}
