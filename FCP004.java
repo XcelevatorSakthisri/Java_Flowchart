fa910ea9ece63b71a06d87fee9fbb756c46975ec
Sri here <sakthisri.2534@xcelevate.org>
2025-07-22 17:16:40 +0530

    Add files via upload

---
+++ FCP004.java
@@ -0,0 +1,18 @@
+public class FCP004{
+public static void main(String[] args){
+int a=12;
+int b=5;
+int add=a+b;
+int sub=a-b;
+int mul=a*b;
+int FD=a/b;
+int mod=a%b;
+double div=(double)a/b;
+System.out.println(add);
+System.out.println(sub);
+System.out.println(mul);
+System.out.println(div);
+System.out.println(mod);
+System.out.println(FD);
+}
+}
