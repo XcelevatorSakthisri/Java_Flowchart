d80b1452b8406614fbd03d1cccd80acd0dcab18a
Sri here <sakthisri.2534@xcelevate.org>
2025-07-22 17:14:41 +0530

    Add files via upload

---
+++ FCP001.java
@@ -0,0 +1,10 @@
+public class FCP001{
+public static void main(String[] args){
+int DD=22;
+String MM="May";
+int YY=2001;
+System.out.println(DD);
+System.out.println(MM);
+System.out.println(YY);
+}
+}
