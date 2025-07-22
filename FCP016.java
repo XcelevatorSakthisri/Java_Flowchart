f7a55dd35754ab97a6b2afc7bbcd6820452dad56
Sri here <sakthisri.2534@xcelevate.org>
2025-07-22 23:28:59 +0530

    Add files via upload

---
+++ FCP016.java
@@ -0,0 +1,20 @@
+public class FCP016{
+public static void main(String[] args){
+int Y=2019;
+if (Y%400==0){
+System.out.println("YES");
+}
+else if (Y%100==0){
+System.out.println("NO");
+}
+else if(Y%4==0){
+System.out.println("YES");
+}
+else
+{
+System.out.println("NO");
+}
+}
+}
+
+      
