9c9dab69519679e39e94341802989f86bd6efb27
Sri here <sakthisri.2534@xcelevate.org>
2025-07-30 16:36:43 +0530

    Add files via upload

---
+++ FCP012.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class FCP012{
+public static void main(String[] args){
+Scanner input=new Scanner(System.in);
+int a=input.nextInt();
+if(a==0){
+System.out.println("0");
+}
+else if(a>0){
+System.out.println("Positive");
+}
+else
+{
+System.out.println("Negative");
+}
+}
+}
