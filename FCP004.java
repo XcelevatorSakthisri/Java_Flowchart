bd2b43f71078e8b487540d082b81c756b360a41c
Sri here <sakthisri.2534@xcelevate.org>
2025-07-30 11:57:42 +0530

    Add files via upload

---
+++ FCP004.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class FCP003{
+public static void main(String[] args){
+Scanner input=new Scanner(System.in);
+int a=input.nextInt();
+int b=input.nextInt();
+int add=a+b;
+int sub=a-b;
+int mul=a*b;
+int FD=a/b;
+int mod=a%b;
+double div=(double)a/b;
+System.out.println(add);
+System.out.println(sub);
+System.out.println(mul);
+System.out.println(div);
+System.out.println(mod);
+System.out.println(FD);
+}
+}
