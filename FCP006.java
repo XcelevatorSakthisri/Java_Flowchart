65570a4b06e58133c95b5e6877863a31e5e9ceab
Sri here <sakthisri.2534@xcelevate.org>
2025-07-30 12:49:21 +0530

    Add files via upload

---
+++ FCP006.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class FCP006{
+public static void main(String[] args){
+Scanner input=new Scanner(System.in);
+int N=input.nextInt();
+int LastDigit=N%10;
+System.out.println(LastDigit);
+}
+}
