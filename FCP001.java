2c41b3fa82edfb2d8348a72110377fba5752d3b9
Sri here <sakthisri.2534@xcelevate.org>
2025-07-30 11:29:13 +0530

    Add files via upload

---
+++ FCP001.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class FCP001{
+public static void main(String[] args){
+Scanner input=new Scanner(System.in);
+int DD=input.nextInt();
+input.nextLine();
+String MM=input.nextLine();
+int YY=input.nextInt();
+System.out.println(DD);
+System.out.println(MM);
+System.out.println(YY);
+}
+}
