052ed9182e24c4b138f0e08820cd1a9073b03859
Sri here <sakthisri.2534@xcelevate.org>
2025-07-30 16:15:18 +0530

    Add files via upload

---
+++ FCP011.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class FCP011{
+public static void main(String[] args){
+Scanner input=new Scanner(System.in);
+int num=input.nextInt();
+if (num%2==0)
+{
+System.out.println("Even");
+}
+else
+{
+System.out.println("Odd");
+}
+}
+}
