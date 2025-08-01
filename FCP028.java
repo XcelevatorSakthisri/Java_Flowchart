55388f0ac115829e4ce2eb3ed71f6ef3fd8d25cf
Sri here <sakthisri.2534@xcelevate.org>
2025-08-02 01:26:01 +0530

    Add files via upload

---
+++ FCP028.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class FCP028{
+public static void main(String[] args){
+Scanner input=new Scanner(System.in);
+int n=input.nextInt();
+if(n==0){
+System.out.println("0");
+}
+else if(n<10){
+System.out.println("1");
+}
+else if(n<100){
+System.out.println("2");
+}
+else if(n<1000){
+System.out.println("3");
+}
+else if(n<10000){
+System.out.println("4");
+}
+else if(n<100000){
+System.out.println("5");
+}
+else if(n<1000000){
+System.out.println("6");
+}
+}
+}
+
+
+
+
+
